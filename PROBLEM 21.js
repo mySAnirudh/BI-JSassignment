02e5669c85eeb12c9801bfe16625599efe81e738
Anirudh Kumar <96437957+mySAnirudh@users.noreply.github.com>
2022-01-07 21:46:31 +0530

    Add files via upload

---
+++ PROBLEM 21.js
@@ -0,0 +1,29 @@
+// 21. Compound Assignment With Augmented Division:
+// The /= operator divides a variable by another number.
+// myVar = myVar / 5;
+// Will divide myVar by 5. This can be rewritten as:
+// myVar /= 5;
+// Convert the assignments for a, b, and c to use the /= operator.
+
+
+// Let:-
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a = a / 12;
+b = b / 4;
+c = c / 11;
+
+// Answer:-
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a /= 12;
+b /= 4;
+c /= 11;
---
+++ PROBLEM 22.js
@@ -0,0 +1,16 @@
+// 22. Declare String Variables:
+// Previously we have used the code
+// var myName = "your name";
+// "your name"is called a string literal. It is a string because it is a series of zero or
+// more characters enclosed in single or double quotes.
+
+// Create two new string variables: myFirstName and myLastName and assign them
+// the values of your first and last name, respectively.
+
+var myFirstName = "Samrat"; // declaring a variable myFirstName and storing a string "Shabana asmin".
+var myLastName = "Singh";// declaring a variable myLastName and storing a string "Mohammad Kalesha".
+
+
+
+
+
